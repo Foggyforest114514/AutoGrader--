ea2924dd191c519642265dfa9b5f4779b137dbc9
skywalkerwhack <159442614+skywalkerwhack@users.noreply.github.com>
2026-05-28 19:38:54 +0800

    On branch smoke_test_and_b3_update (#7)
    Co-authored-by: bart_dude <kanye@seek.li>

--- httpie_smoke_all.sh
+++ httpie_smoke_all.sh
@@ -5,6 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "${ROOT_DIR}/httpie_b3_tests.sh"
 "${ROOT_DIR}/httpie_b4_tests.sh"
+"${ROOT_DIR}/httpie_b4_extended_tests.sh"
 "${ROOT_DIR}/httpie_b2_tests.sh"
 
 echo "All httpie smoke tests passed"
